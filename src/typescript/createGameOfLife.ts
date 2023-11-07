2f8238aa2b1c7a55d17a28fc5c232296f8364092
Vladislav <vidga471@gmail.com>
2023-11-07 07:34:21 +0300

    add speed game

--- src/typescript/createGameOfLife.ts
+++ src/typescript/createGameOfLife.ts
@@ -8,6 +8,10 @@ export function createGameOfLife(sizeX: number, sizeY: number, htmlElement: HTML
   let timer: number | undefined;
 
   htmlElement.innerHTML = `
+    <div id="speed-control">
+    <label for="speedRange" style="display: block; font-size: 20px; text-align: center;">Скорость игры</label>
+    <input type="range" id="speedRange" min="1" max="1000" value="30" style="width: 50%;" />
+    </div>
     <button id="resize">RESIZE</button>
     <input type="number" id="sizeX" placeholder="Кол-во колонок" value="${sizeX}" />
     <input type="number" id="sizeY" placeholder="Кол-во строк" value="${sizeY}" />
@@ -15,6 +19,7 @@ export function createGameOfLife(sizeX: number, sizeY: number, htmlElement: HTML
     <button id="start">START</button>
   `;
 
+  const speedRange: HTMLInputElement = htmlElement.querySelector("#speedRange")! as HTMLInputElement;
   const fieldWrapper: HTMLElement = htmlElement.querySelector(".field-wrapper")!;
   const buttonStart: HTMLButtonElement = htmlElement.querySelector("#start")! as HTMLButtonElement;
   const buttonResize: HTMLButtonElement = htmlElement.querySelector("#resize")! as HTMLButtonElement;
@@ -30,13 +35,6 @@ export function createGameOfLife(sizeX: number, sizeY: number, htmlElement: HTML
     drawField(fieldWrapper, field, cellClickHandler);
   };
 
-  const resizeField = () => {
-    sizeX = parseInt(inputSizeX.value, 10);
-    sizeY = parseInt(inputSizeY.value, 10);
-    field = Array.from({ length: sizeY }, () => Array.from({ length: sizeX }, () => 0));
-    drawField(fieldWrapper, field, cellClickHandler);
-  };
-
   drawField(fieldWrapper, field, cellClickHandler);
 
   function stopGame(): void {
@@ -45,6 +43,12 @@ export function createGameOfLife(sizeX: number, sizeY: number, htmlElement: HTML
     if (timer) {
       clearInterval(timer);
     }
+  }
+
+  function calculateInterval(speedValue: number): number {
+    // Преобразуем значение ползунка так, чтобы 1 соответствовало 100 мс, а 1000 — 1000 мс.
+    // Предполагаем, что speedValue находится в диапазоне [1, 1000].
+    return 1100 - speedValue;
   }
 
   function startGame(): void {
@@ -57,8 +61,32 @@ export function createGameOfLife(sizeX: number, sizeY: number, htmlElement: HTML
         alert("Все клетки мертвы");
         stopGame();
       }
-    }, 1000);
+    }, calculateInterval(speedRange.valueAsNumber));
   }
+
+  const resizeField = () => {
+    sizeX = parseInt(inputSizeX.value, 10);
+    sizeY = parseInt(inputSizeY.value, 10);
+    field = Array.from({ length: sizeY }, () => Array.from({ length: sizeX }, () => 0));
+    drawField(fieldWrapper, field, cellClickHandler);
+    if (gameIsRunning) {
+      stopGame();
+      startGame();
+    }
+  };
+
+  speedRange.addEventListener('input', () => {
+    if (gameIsRunning && timer) {
+      clearInterval(timer);
+      timer = window.setInterval(() => {
+        field = getNextState(field);
+        drawField(fieldWrapper, field, cellClickHandler);
+        if (!isAnyoneAlive(field)) {
+          stopGame();
+        }
+      }, calculateInterval(speedRange.valueAsNumber));
+    }
+  });
 
   buttonStart.addEventListener("click", () => {
     if (!gameIsRunning) {
